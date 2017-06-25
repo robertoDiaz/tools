d15195fa41cb7404f20dea2d9852aaf630705a15
Roberto Díaz <robertodimen@gmail.com>
2017-06-25 02:47:32 +0200

    Add checkout script v0.2

--- checkout.py
+++ checkout.py
@@ -1,24 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+#
+# author Roberto Díaz
+#
+# Requires  gitpython.
+#
+# To install it run "sudo pip install gitpython"
+#
+# Instructions:
+# 
+# Requires 1 parameter, the remote branch or tag. Has a second optional that allows
+# set the local branch name. If not used, the remote branch name will be used as
+# local branch name.
+
 import os
 import sys
+
 from git import Repo
 from git import Remote
     
-if len(sys.argv) == 2:
-	remoteBranch = sys.argv[1]
-	localBranch = sys.argv[1]
-elif len(sys.argv) == 3:
-	remoteBranch = sys.argv[1]
-	localBranch = sys.argv[2]
-else :
-	raise Exception('Wrong parameters')
 
-repo = Repo('.')
-remote = Remote(repo, 'upstream')
+def checkout(remoteBranch, localBranch):
+	repo = Repo('.')
 
-print('Fetching ' + remoteBranch)
+	print('Fetching ' + remoteBranch)
 
-remote.fetch('refs/tags/' + remoteBranch)
+	Remote(repo, 'upstream').fetch('refs/tags/' + remoteBranch)
 
-print('Creating  and checking out ' + localBranch)
+	print('Creating  and checking out ' + localBranch)
 
-repo.create_head(localBranch, 'FETCH_HEAD').checkout()
+	repo.create_head(localBranch, 'FETCH_HEAD').checkout()
+
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		remoteBranch = sys.argv[1]
+		localBranch = sys.argv[1]
+	elif len(sys.argv) == 3:
+		remoteBranch = sys.argv[1]
+		localBranch = sys.argv[2]
+	else :
+		raise Exception('Wrong parameters')
+
+	checkout(remoteBranch, localBranch)
